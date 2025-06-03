e2444e5b22041170d404770b798a07a6944f3bbc
2983266930 <2983266930@qq.com>
2025-06-03 17:26:23 +0800

    首次提交

---
+++ src/.vuepress/config.ts
@@ -0,0 +1,16 @@
+import { defineUserConfig } from "vuepress";
+
+import theme from "./theme.js";
+
+export default defineUserConfig({
+  base: "/",
+
+  lang: "zh-CN",
+  title: "shawya的技术博客",
+  description: "这是shawya的技术博客",
+
+  theme,
+
+  // 和 PWA 一起启用
+  // shouldPrefetch: false,
+});
---
+++ src/intro.md
@@ -0,0 +1,8 @@
+---
+icon: circle-info
+cover: /assets/images/cover3.jpg
+---
+
+# 介绍页
+
+尝试构建个人网站的新人一枚，有错误欢迎大家指出
