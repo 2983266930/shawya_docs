1ca96a0db67629186b2f8e0f7b01d5a3486f2352
2983266930 <2983266930@qq.com>
2025-06-03 18:06:37 +0800

    改base

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/shawya_docs/",
 
   lang: "zh-CN",
   title: "shawya的技术博客",
